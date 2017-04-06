4701d654f4f312911b3c217d1c1fbf3dc927bc21
Irving Mancera Robles <irvingmanrob@hotmail.com>
2017-04-06 19:39:18 +0000

    Creracion de roles de usuario

---
+++ HSBC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HSBC.Startup))]
+namespace HSBC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
